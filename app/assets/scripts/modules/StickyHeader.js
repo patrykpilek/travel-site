c59bc77c60eeec5968d27be5a878f4d846301c76
Patryk Pilek <patryk.pilek@gmail.com>
2017-08-06 00:29:32 +0100

    Completed lazy loading of images.

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -4,6 +4,7 @@ import smoothScroll from 'jquery-smooth-scroll';
 
 class Stickyheader {
     constructor() {
+        this.lazyImages = $('.lazyload');
         this.siteHeader = $(".site-header");
         this.headerTriggerElement = $(".large-hero__title");
         this.createHeaderWaypoint();
@@ -11,6 +12,13 @@ class Stickyheader {
         this.headerLinks = $(".primary-nav a");
         this.createPageSectionWaypoints();
         this.addSmoothScrolling();
+        this.refreshWayponts();
+    }
+
+    refreshWayponts() {
+        this.lazyImages.on('load', function() {
+            Waypoint.refreshAll();
+        });
     }
 
     addSmoothScrolling() {
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11266,6 +11266,7 @@ var Stickyheader = function () {
     function Stickyheader() {
         _classCallCheck(this, Stickyheader);
 
+        this.lazyImages = (0, _jquery2.default)('.lazyload');
         this.siteHeader = (0, _jquery2.default)(".site-header");
         this.headerTriggerElement = (0, _jquery2.default)(".large-hero__title");
         this.createHeaderWaypoint();
@@ -11273,9 +11274,17 @@ var Stickyheader = function () {
         this.headerLinks = (0, _jquery2.default)(".primary-nav a");
         this.createPageSectionWaypoints();
         this.addSmoothScrolling();
+        this.refreshWayponts();
     }
 
     _createClass(Stickyheader, [{
+        key: 'refreshWayponts',
+        value: function refreshWayponts() {
+            this.lazyImages.on('load', function () {
+                Waypoint.refreshAll();
+            });
+        }
+    }, {
         key: 'addSmoothScrolling',
         value: function addSmoothScrolling() {
             this.headerLinks.smoothScroll();
